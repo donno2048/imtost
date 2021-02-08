69c5a7b5994251d806d49870ea4946307dbadd71
Elisha Hollander <just4now666666@gmail.com>
2021-02-08 22:21:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function start(client) {
   client.onMessage(async (message) => {
     if (message.isMedia) {
       const filename = `./input.${message.mimetype.split('/')[1]}`
-      fs.writeFileSync(filename, await decryptMedia(message, process.env.UserAgent))
+      fs.writeFileSync(filename, await decryptMedia(message, process.env.UserAgent).catch())
       ffmpeg(filename).size('240x?').aspect('1:1').autopad().fps(30).output('./output.webp').on('end', function () {ffmpeg(filename).size(`240x?`).fps(`${Math.floor(30 * (900000 / fs.statSync('./output.webp').size)) - 1}`).aspect('1:1').autopad().output('./output.webp').on('end', function () {client.sendImageAsStickerGif(message.from, './output.webp')}).run()}).run()
     }
   })
